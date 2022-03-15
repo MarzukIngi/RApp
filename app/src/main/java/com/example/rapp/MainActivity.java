823f1044a023ae0ce7f0d23a196e326426bf7588
Gitcelo <mfa5@hi.is>
2022-03-15 21:01:09 +0000

    Held að ég sé búinn að útfæra að fara úr MainActivity í önnur acivity

--- app/src/main/java/com/example/rapp/MainActivity.java
+++ app/src/main/java/com/example/rapp/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.rapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button mRecipesButton;
+    private Button mSignUpButton;
+    private Button mLogInButton;
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +24,27 @@ public class MainActivity extends AppCompatActivity {
         mRecipesButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                System.out.println("Færist yfir í uppskriftir");
+                Log.d(TAG, "Færist yfir í uppskriftir");
+                Intent intent = new Intent(MainActivity.this, RecipeMainActivity.class);
+                startActivity(intent);
+            }
+        });
+        mSignUpButton = (Button) findViewById(R.id.signup_button);
+        mSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "Færist yfir í signup");
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+        mLogInButton = (Button) findViewById(R.id.login_button);
+        mLogInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               Log.d(TAG, "Færist yfir í login");
+               Intent intent = new Intent(MainActivity.this, LogInActivity.class);
+               startActivity(intent);
             }
         });
     }
