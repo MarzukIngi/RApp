bc2e93dd8bbe63788f181461275dfa2681b23c5e
Gitcelo <mfa5@hi.is>
2022-03-15 17:57:27 +0000

    Búinn að laga activity skjöl

---
+++ app/src/main/java/com/example/rapp/PageActivities/FrontPageActivity.java
@@ -0,0 +1,14 @@
+package com.example.rapp.PageActivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FrontPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_front_page);
+    }
+}
