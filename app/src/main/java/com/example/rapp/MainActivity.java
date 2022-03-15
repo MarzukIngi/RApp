826079fa1d9ae250b8736746052cdcf175ba7f24
Gitcelo <mfa5@hi.is>
2022-03-15 20:00:25 +0000

    Byrjaður að setja upp takka á main activity

--- app/src/main/java/com/example/rapp/MainActivity.java
+++ app/src/main/java/com/example/rapp/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.rapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mRecipesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRecipesButton = (Button) findViewById(R.id.recipes_button);
+        mRecipesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("Færist yfir í uppskriftir");
+            }
+        });
     }
 }
