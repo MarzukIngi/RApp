0ad207e7e84db09d32c5dac9e9973ba842282a11
Gitcelo <72700939+Gitcelo@users.noreply.github.com>
2022-03-16 14:40:41 +0000

    Aðeins búinn að laga upphafstengingarnar

--- app/src/main/java/com/example/rapp/MainActivity.java
+++ app/src/main/java/com/example/rapp/MainActivity.java
@@ -19,11 +19,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mRecipesButton = (Button) findViewById(R.id.recipes_button);
+        mRecipesButton = (Button) findViewById(R.id.front_page_button);
         mRecipesButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, RecipeMainActivity.class);
+                Intent intent = new Intent(MainActivity.this, FrontPageActivity.class);
                 startActivity(intent);
             }
         });
