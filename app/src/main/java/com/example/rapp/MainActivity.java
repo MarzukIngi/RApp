7c4e2cfdfb26374a749590f97c18c5f135dd2e1f
Gitcelo <72700939+Gitcelo@users.noreply.github.com>
2022-04-15 18:55:47 +0000

    Nav komið upp

--- app/src/main/java/com/example/rapp/MainActivity.java
+++ app/src/main/java/com/example/rapp/MainActivity.java
@@ -2,15 +2,9 @@ package com.example.rapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button mRecipesButton;
-    private Button mLogInButton;
 
     private static final String TAG = "MainActivity";
 
@@ -18,21 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mRecipesButton = (Button) findViewById(R.id.front_page_button);
-        mRecipesButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, FrontPageActivity.class);
-                startActivity(intent);
-            }
-        });
-        mLogInButton = (Button) findViewById(R.id.login_button);
-        mLogInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-               Intent intent = new Intent(MainActivity.this, LogInActivity.class);
-               startActivity(intent);
-            }
-        });
+
     }
 }
